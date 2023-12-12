2a8dd0ba51137de19423f14d1d9a1495b67d7668
unknown <C:\Users\\AppData\Roaming\The Bat!>
2023-12-12 09:51:56 +0500

    add

--- angular-components/src/app/app.component.ts
+++ angular-components/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-components';
+
+  posts: Post[] = [
+    { title: 'Хочу выучить Ангуляр компоненты', text: 'Я все еще учу', id: 1 },
+    {
+      title: 'Следующий блок',
+      text: 'будет про директивы и еще про пайпы',
+      id: 2,
+    },
+    { title: 'Следующий блок', text: 'потом еще что нибудь выучу', id: 3 },
+  ];
+
+  updatePosts(post: Post) {
+    this.posts.unshift(post);
+    console.log('Post', post);
+  }
 }
--- angular-components/src/app/post-form/post-form.component.ts
+++ angular-components/src/app/post-form/post-form.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { Post } from '../app.component';
 
 @Component({
   selector: 'app-post-form',
   templateUrl: './post-form.component.html',
-  styleUrls: ['./post-form.component.scss']
+  styleUrls: ['./post-form.component.scss'],
 })
-export class PostFormComponent {
+export class PostFormComponent implements OnInit {
+  @Output() onAdd: EventEmitter<Post> = new EventEmitter<Post>();
 
+  @ViewChild('titleInput') inputRef!: ElementRef;
+
+  title = '';
+  text = '';
+
+  constructor() {}
+  ngOnInit(): void {
+
+  }
+
+  addPosts() {
+    if (this.text.trim() && this.title.trim()) {
+      const post: Post = {
+        title: this.title,
+        text: this.text,
+      };
+
+      this.onAdd.emit(post);
+      this.title = this.text = '';
+    }
+  }
+
+  focusTitle() {
+    this.inputRef.nativeElement.focus();
+  }
 }
