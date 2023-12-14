a9b1a5132a9fa0acb3253d6041a9e10dfafc04ab
unknown <C:\Users\\AppData\Roaming\The Bat!>
2023-12-14 12:40:53 +0500

    add

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { StyleDirective } from './directives/style.directive';
+import { Style2Directive } from './directives/style2.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StyleDirective,
+    Style2Directive
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
